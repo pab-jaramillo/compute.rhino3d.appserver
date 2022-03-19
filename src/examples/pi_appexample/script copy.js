2386b7d01644ba7f6f7192db7258304913205454
Pablo Jaramillo <97456531+pab-jaramillo@users.noreply.github.com>
2022-03-19 18:51:59 -0500

    New GH File

--- src/examples/pi_appexample/script copy.js
+++ src/examples/pi_appexample/script copy.js
@@ -9,7 +9,7 @@ import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm
 const loader = new Rhino3dmLoader()
 loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
-const definition = 'PI_Example_ModuleAggGr'
+const definition = 'PI_RecreationModule.gh'
 
 // setup input change events
 const inhabitants_slider = document.getElementById( 'Number_People' )
