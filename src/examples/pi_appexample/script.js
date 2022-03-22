3d64a780a83a4a2e30ace93dbfff8442d607c587
pab-jaramillo <97456531+pab-jaramillo@users.noreply.github.com>
2022-03-22 16:19:11 -0500

    changes final 04

--- src/examples/pi_appexample/script.js
+++ src/examples/pi_appexample/script.js
@@ -79,7 +79,7 @@ function init() {
     camera.position.set(1, -1, 1) // like perspective view
 
     // very light grey for background, like rhino
-    scene.background = new THREE.Color('transparent')
+    scene.background = new THREE.Color('black')
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer({ antialias: true })
